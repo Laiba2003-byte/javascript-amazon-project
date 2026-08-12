157e8dbbf1d6475fa10672ddc251058a6251fe08
Laiba Ajmal <laibaajmal276@gmail.com>
2026-08-12 11:12:34 +0500

    class principles on cart

---
+++ data/cart-class.js
@@ -0,0 +1,78 @@
+class Cart {
+
+  cartItems=undefined;
+  localStorageKey= undefined
+
+   loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+    }
+
+    addToCart(productId) {
+      let matchingItem;
+
+      this.cartItems.forEach((item) => {
+        if (item.productId === productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        this.cartItems.push({
+          productId: productId,
+          quantity: 1,
+          deliveryOptionId: 1
+        });
+      }
+
+      this.saveToStorage();
+    }
+
+    removeFromCart(productId) {
+      this.cartItems = this.cartItems.filter((item) => {
+        return item.productId !== productId;
+      });
+
+      this.saveToStorage();
+    }
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+
+      this.cartItems.forEach((item) => {
+        if (item.productId === productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.deliveryOptionId = deliveryOptionId;
+      }
+
+      this.saveToStorage();
+    }
+
+    getCartQuantity() {
+      let cartQuantity = 0;
+
+      this.cartItems.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      return cartQuantity;
+    }
+
+    saveToStorage() {
+      localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    }
+}
+
+
+
+const cart= new Cart () ;
+const cartBusiness= new Cart () ;
+cart.localStorageKey = 'cart-oop';
+cartBusiness.localStorageKey = 'cart-business';
+
+export { cart, cartBusiness };
