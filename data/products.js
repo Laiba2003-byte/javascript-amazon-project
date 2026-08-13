5c695780e51693c3fcdcdd1b62992bfbb2f52a7e
Laiba Ajmal <laibaajmal276@gmail.com>
2026-08-13 13:07:39 +0500

    class implmentation

--- data/products.js
+++ data/products.js
@@ -1,3 +1,28 @@
+
+import { formatCurrency } from "../scripts/utils/money"
+class Product {
+    id
+    image
+    name
+    rating
+    priceCents
+
+    constructor(productData) {
+        this.id = productData.id;
+        this.image = productData.image;
+        this.name = productData.name;
+        this.rating = productData.rating;
+        this.priceCents = productData.priceCents;
+    }
+    getstarsUrl()
+    {
+      return  `images/ratings/rating-${this.rating.stars * 10}.png`
+    }
+    getPrice()
+    {
+      return `$${formatCurrency(this.priceCents)}`;    }
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -658,4 +683,9 @@ export const products = [
       "mens"
     ]
   }
-];
+].map( (productDetails)=>
+{
+  new Product(productDetails);
+}
+
+);
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -20,7 +20,7 @@ products.forEach((product) => {
           <div class="product-rating-container">
              <img
                 class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars * 10}.png">
+                src=${product.getstarsUrl()}>
            
 
             <div class="product-rating-count link-primary">
@@ -29,7 +29,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${formatCurrency(product.priceCents)}
+            ${product.getPrice()}
           </div>
 
           <div class="product-quantity-container">
