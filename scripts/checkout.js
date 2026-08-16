a3d1129942a5ba4637fca01a7686a77f3de6f139
laiba-ajmal-12 <laibaajmal276@gmail.com>
2026-08-16 17:01:11 +0500

    promises and promise all

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,61 @@
 import { renderSummary } from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from './cart.js';
 
+new Promise.all([
 
-loadProducts(  ()=>
+  newPromise((resolve)=>
+{
+  loadProducts(()=>
+  {
+      resolve();
+
+  });
+}),
+ new Promise((resolve)=>
+  {
+    loadCart(()=>
+    {
+      resolve();
+    })
+  })
+
+]).then (()=>
 {
   renderSummary();
   renderPaymentSummary();
-
 })
+
+
+// newPromise((resolve)=>
+// {
+//   loadProducts(()=>
+//   {
+//       resolve();
+
+//   });
+// }).then(()=>
+// {
+//   return new Promise((resolve)=>
+//   {
+//     loadCart(()=>
+//     {
+//       resolve();
+//     })
+//   })
+// }).then (()=>
+// {
+//   renderSummary();
+//   renderPaymentSummary();
+// })
+
+// without prmosie too much nesting
+// loadProducts(  ()=>
+// {
+//   loadCart(()=>
+//   {
+//     renderSummary();
+//     renderPaymentSummary();
+//   })
+// })
